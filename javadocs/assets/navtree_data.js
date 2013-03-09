56c99cf0273d798d52ada5aeb29bdea6f298717d
Moxie Marlinspike <moxie@thoughtcrime.org>
2013-03-09 10:22:03 -0800

    Remove old javadocs

--- javadocs/assets/navtree_data.js
+++
@@ -1,7 +0,0 @@
-var NAVTREE_DATA =
-[ [ "org.thoughtcrime.ssl.pinning", "org/thoughtcrime/ssl/pinning/package-summary.html", [ [ "Description", "org/thoughtcrime/ssl/pinning/package-descr.html", null, "" ], [ "Classes", null, [ [ "PinningTrustManager", "org/thoughtcrime/ssl/pinning/PinningTrustManager.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
--- javadocs/reference/lists.js
+++
@@ -1,5 +0,0 @@
-var DATA = [
-      { id:0, label:"org.thoughtcrime.ssl.pinning", link:"org/thoughtcrime/ssl/pinning/package-summary.html", type:"package" },
-      { id:1, label:"org.thoughtcrime.ssl.pinning.PinningTrustManager", link:"org/thoughtcrime/ssl/pinning/PinningTrustManager.html", type:"class" }
-
-    ];
